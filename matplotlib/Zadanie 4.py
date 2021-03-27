ef531e6476f78abb41e02c4ec333532899414125
ghost <79923443+TheMostMrGhost@users.noreply.github.com>
2021-03-27 22:57:46 +0100

    Działa dla jednego kraju, nie obsuługuje błędów

--- matplotlib/Zadanie 4.py
+++ matplotlib/Zadanie 4.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 #Wersja alpha, jeszcze nie ściąga informacji z internetu
 
 #===================================
@@ -9,24 +10,34 @@ def dostepneKraje():
             if x[1]!=poprzedni:
                 print(x[1])
             poprzedni = x[1]
-    #     return
-    # data = []
-    # x = lines[0]
-    # print(lines[0])
-    # print(type(x))
-    # x = x.split(",")
-    # print(x)
-    # for ii in lines:
-    #     data.append((lines[1].split(","))[2])
-    # poprzedni = "location"
-    # for e in data:
-    #     if(e[2] != poprzedni):
-    #         print(e[2])
-    #     poprzedni = e[2]
-    
+#===================================
+def daneZachorowań(kraj): 
+    #Funkcję wywołujemy tylko pod warunkiem, że kraju nie ma w słowniku
+    with open("./full_data.csv") as file:
+        zachorowania = []
+        flaga = False
+        for linia in file:
+            x = linia.split(",")
+            if x[1] != kraj:
+                if flaga==False:
+                    continue
+                else:
+                    return zachorowania
+            else:
+                flaga = True
+                zachorowania.append(int(float(linia.split(",")[2])))             
 #===================================
 #Główny program
 #===================================
 print("Witaj w programie pokazującym liczbę zachorowań na COVID-19 w różnych krajach na świecie.\n Oto lista dostępnych " +
         "krajów:\n")
 dostepneKraje()
+przypadki = {}
+wybrany = input("Wybierz kraj z dstępnych na liście\n")
+chorzy = daneZachorowań(wybrany)
+dni = [ii+1 for ii in range(len(chorzy))]
+# for ee in range(len(chorzy)):
+#     print(dni[ee], chorzy[ee])]
+plt.plot(dni, chorzy)
+plt.show()
+plt.close()
