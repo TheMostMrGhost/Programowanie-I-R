3ae46488d2574ed58d7b7cd8ac869fba0d392201
ghost <79923443+TheMostMrGhost@users.noreply.github.com>
2021-04-05 17:17:30 +0200

    Już wszystko powinno być ok, ewentualnie zrobić testy i dodać jakiś dialog z użytkownikiem

--- matplotlib/projectile.py
+++ matplotlib/projectile.py
@@ -1,4 +1,6 @@
-import matplotlib.pyplot as plt, math, numpy as np
+import matplotlib.pyplot as plt, math, numpy as np, matplotlib.animation as animation
+def dajInterwał():
+    return 0.02
 #======================================
 def wczytajWartości():
     print("Witaj w programie wizualizującym ruch punktu materialnego w rzucie ukośnym")
@@ -7,10 +9,30 @@ def wczytajWartości():
     wynik.update({"wysokość" : float(input("Podaj wysokość z jakiej zrzucane jest ciało:\n"))})
     wynik.update({"współczynnik" : float(input("Podaj współczynnik oporu powietrza:\n"))})
     wynik.update({"początkowa" : float(input("Podaj prędkość początkową:\n"))})
-    wynik.update({"kąt" : float(input("Podaj kąt (liczony od osi OX):\n"))})
+    wynik.update({"kąt" : int(input("Podaj kąt (liczony od osi OX):\n"))})
     return  wynik
 #======================================
-def droga(dane,ile, interwał):
+def trajektoria(dane):
+    interwał = dajInterwał()
+    masa = dane["masa"]
+    wysokość = dane["wysokość"]
+    opór = dane["współczynnik"]
+    szybX = dane["początkowa"]*np.cos(math.radians(dane["kąt"]))
+    szybY = dane["początkowa"]*np.sin(math.radians(dane["kąt"]))
+
+    wysokości = [wysokość]
+    odległości = [0]
+    ii = 1
+    while wysokości[ii-1]>=0:
+        szybY = szybY - np.sign(szybY)*opór*szybY*szybY*interwał/masa - 9.81*interwał
+        wysokości.append(wysokości[ii-1] + szybY*interwał)
+        szybX = szybX - opór*szybX*szybX*interwał/masa
+        odległości.append(odległości[ii-1] + szybX*interwał)
+        ii += 1
+    return wysokości, odległości
+#======================================
+def droga(dane,ile):
+    interwał = interwał()
     def liczX(V0, B, m, ile, interwał):
         for ii in range(ile):
             print(V0)
@@ -24,7 +46,6 @@ def droga(dane,ile, interwał):
 
     szybkoscWPionie = dane["początkowa"]*math.sin( math.radians(dane["kąt"])  )
     szybkoscWPoziomie = np.abs(dane["początkowa"]*math.cos( math.radians( dane["kąt"])) )
-
     print("Szybkość w pionie: {0}\n".format(szybkoscWPionie))
     print("Szybkość w poziomie: {0}\n".format(szybkoscWPoziomie))
     wynikX = list(liczX(szybkoscWPoziomie, dane["współczynnik"], dane["masa"], ile, interwał))
@@ -39,16 +60,37 @@ def droga(dane,ile, interwał):
         ii+=1
     return wysokości, odległości
 #======================================
+def szukajMaksa(dane):#Ta funkcja jest po to żeby móc ładnie ustawić zakres na Y
+    maks = - np.inf
+    for ii in dane:
+        if(ii>maks):
+            maks = ii
+    return maks
+#======================================
 #Część główna programu
 #======================================
-INTERWAŁ = 0.01
+INTERWAŁ = dajInterwał() #Ogarnąć czy ten interwał można dawać jakiś większy czy zawsze taki
 dane = wczytajWartości()
-ile = int(input())
 
-wysokości, odległości = droga(dane, ile,INTERWAŁ)
-for ii in range(len(wysokości)):
-    print("{0}   {1}".format(wysokości[ii], odległości[ii]))
+wysokości, odległości = trajektoria(dane)
 
-fig, ax = plt.subplots()
-ax.plot(odległości, wysokości)
+fig, ax = plt.subplots(tight_layout = True)
+ax.set_xlim(0, 11*odległości[-1]/10)
+ax.set_ylim(0,11*szukajMaksa(wysokości)/10)
+
+obj, = ax.plot([],[],'g', markersize = 3)
+obj2, = ax.plot([],[], 'r.')
+wykres = plt.get_current_fig_manager()
+wykres.window.showMaximized()
+xdata = []
+ydata = []
+
+def dajDane(i):
+    xdata.append(odległości[i])
+    ydata.append(wysokości[i])
+    obj.set_data(xdata,ydata)
+    obj2.set_data(odległości[i],wysokości[i])
+    return obj, obj2
+
+ani = animation.FuncAnimation(fig,dajDane,len(wysokości),interval = 20/len(wysokości), blit = True, repeat = False)
 plt.show()
