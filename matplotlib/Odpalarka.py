3c6afc1dc63aa67d632c03411685ed64b3a1f9de
ghost <79923443+TheMostMrGhost@users.noreply.github.com>
2021-04-13 20:03:28 +0200

    Wersja finalna

--- matplotlib/Odpalarka.py
+++ matplotlib/Odpalarka.py
@@ -1,5 +1,10 @@
 import projectile
+odKtorego = int(input("Od którego pomiaru zacząć?\n"))
+ii = 1
 with open("./Dane do rzutów.csv") as file:
     for linia in file:
+        ii +=1
         x = linia.split(",")
-        projectile.main(x)
+        if ii >odKtorego:
+            projectile.main(x)
+        
